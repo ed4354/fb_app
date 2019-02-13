57f9888233e26066559f0e1fdd333c81bba0f3da
ed4354 <eddli.sac@gmail.com>
2019-02-12 23:20:17 -0800

    passport-facebook package

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-// Create a const that gets the package express(same as import in golang)
-const express = require('express')
-// Create a const that lets you use the express package 
-const app = express()
+const express = require("express");
+const passport = require("passport");
+const FacebookStrategy = require("passport-facebook").Strategy;
+const app = express();
+const port = 3000;
 
-const port = 3000
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: "2287013654662872",
+      clientSecret: "38b7035159d30af1ec7e0b89cc43a794",
+      callbackURL: "http://localhost:3000/auth/facebook/callback"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      //   return cb(err, user);
+      // });
+      // if (profile) {
+      //   facebookId = profile.id;
+      //   return cb(null, user);
+      // } else {
+      //   return done(null, false);
+      // }
+      console.log("profile ", profile)
+      return cb(null, false)
+    }
+  )
+);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/auth/facebook", passport.authenticate("facebook"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  function(req, res) {
+    console.log("req - ", req);
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
